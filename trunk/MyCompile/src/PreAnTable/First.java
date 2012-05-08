a5000415ac947abe10af794aefd5f195045dfbb8
huhaixiao1990@gmail.com <huhaixiao1990@gmail.com@e6c799d9-3fb6-f3f2-b587-402600d2527e>
2012-05-08 09:54:25 +0000

    大范围的重构，给出一些测试。

--- trunk/MyCompile/src/PreAnTable/First.java
+++ trunk/MyCompile/src/PreAnTable/First.java
@@ -8,7 +8,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 
-import PreAnTable.CommProcess.TokenProcesser;
+import PreAnTable.CommProcess.Grammar;
 
 /**
  * 求first集
@@ -20,28 +20,27 @@ import PreAnTable.CommProcess.TokenProcesser;
  */
 public class First {
 	/** 非终结符之间的依赖关系 */
-	Map<String, Set<String>> rela;
+	Map<Integer, Set<Integer>> rela;
 	/** first集合 */
-	Map<String, Set<String>> first_set;
-	/** 产生式 */
-	Map<String, String[]> pros;
+	Map<Integer, Set<Integer>> first_set;
 	/** 记录某个非终结符是否为ε */
-	Map<String, Boolean> isEpsilon;
-
+	Map<Integer, Boolean> isEpsilon;
+	Grammar G;
 	public static void main(String[] args) {
 		First f = new First();
-		String[] input = { "E->TE'", "E'->+TE'|ε", "T->FT'", "T'->*FT'|ε",
-				"F->(E)|i" };
-		f.SetGrammar(input);
+		String[] input = { "<E>-><T><E'>", "<E'>->+<T><E'>| ε",
+				"<T>-><F><T'>","<T'>->*<F><T'>| ε","<F>->(<E>) | i" };
+		Grammar G=CommProcess.ProcessProd(input,false);
+		f.SetGrammar(G);
 		f.analyse();
-		Map<String, Set<String>> first_set = f.getUnTermFirstSet();
-		for (Entry<String, Set<String>> s : first_set.entrySet()) {// 遍历每一个非终结符
-			System.out.print("First(" + s.getKey() + ")={");
-			String ss;
-			for (Iterator<String> iterator = s.getValue().iterator(); iterator
+		Map<Integer, Set<Integer>> first_set = f.getUnTermFirstSet();
+		for (Entry<Integer, Set<Integer>> s : first_set.entrySet()) {// 遍历每一个非终结符
+			System.out.print("First(<" + G.NTs[s.getKey()] + ">)={");
+			Integer ss;
+			for (Iterator<Integer> iterator = s.getValue().iterator(); iterator
 					.hasNext();) {
 				ss = iterator.next();
-				System.out.print(ss);
+				System.out.print(G.Ts[ss]);
 				if (iterator.hasNext()) {
 					System.out.print(",");
 				} else {
@@ -51,18 +50,14 @@ public class First {
 		}
 	}
 
-	public void SetGrammar(String[] ps) {
-		SetGrammar(CommProcess.Prod(ps));
-	}
-
-	public void SetGrammar(Map<String, String[]> pros) {
+	public void SetGrammar(Grammar G) {
+		this.G=G;
 		rela = new HashMap<>();
 		first_set = new HashMap<>();
-		this.pros = pros;
 		isEpsilon = new HashMap<>();
-		for (String s : pros.keySet()) {
-			first_set.put(s, new HashSet<String>());
-			isEpsilon.put(s, null);
+		for (int i = 0; i < G.NTs.length; i++) {
+			first_set.put(i, new HashSet<Integer>());
+			isEpsilon.put(i, null);
 		}
 		analyse();
 	}
@@ -72,7 +67,7 @@ public class First {
 	 * 
 	 * @return
 	 */
-	public Map<String, Set<String>> getUnTermFirstSet() {
+	public Map<Integer, Set<Integer>> getUnTermFirstSet() {
 		return first_set;
 	}
 
@@ -82,39 +77,37 @@ public class First {
 	 * @param pr
 	 * @return
 	 */
-	public Set<String> getStringFirstSet(String pr) {
-		Set<String> fi = new HashSet<>();
-		Set<String> dep = new HashSet<>();
-		TokenProcesser tr = new TokenProcesser(pr);
-		while (tr.hasNextToken()) {
-			String t = tr.nextToken();
-			if (CommProcess.IsTerminal(t)) {// 终结符
-				fi.add(t);// 向first集合中添加元素
+	public Set<Integer> getStringFirstSet(Integer[] pr,int start) {
+		Set<Integer> fi = new HashSet<>();
+		Set<Integer> dep = new HashSet<>();
+		for (int t=start;t<pr.length;t++) {
+			if(pr[t]<0){// 终结符
+				fi.add(-pr[t]);// 向first集合中添加元素
 				return fi;
-			} else {// 非终结符
+			}else{// 非终结符
 				// 添加依赖项
-				dep.add(t);// 此依赖于t
-				if (!inferUnTermEpsilon(t)) {// 此非终结符不可能为ε，不必再向下搜索了
+				dep.add(pr[t]);// 此依赖于t
+				if (!inferUnTermEpsilon(pr[t])) {// 此非终结符不可能为ε，不必再向下搜索了
 					break;
-				} else if (!tr.hasNextToken()) {// 即最后一个非终结符，且可能为空
-					fi.add("ε");
+				} else if (t==pr.length-1) {// 即最后一个非终结符，且可能为空
+					fi.add(CommProcess.Epsilon);
 				}
 			}
 		}
-		for (String d : dep) {
-			Set<String> its = first_set.get(d);
+		for (Integer d : dep) {
+			Set<Integer> its = first_set.get(d);
 			UnionWithoutEpsilon(its, fi);
 		}
 		return fi;
 	}
 
-	public static void UnionWithoutEpsilon(Set<String> src, Set<String> des) {
-		boolean epsilon = src.contains("ε");
+	public static void UnionWithoutEpsilon(Set<Integer> src, Set<Integer> des) {
+		boolean epsilon = src.contains(CommProcess.Epsilon);
 		if (epsilon)
-			src.remove("ε");// 暂时移除ε
+			src.remove(CommProcess.Epsilon);// 暂时移除ε
 		des.addAll(src);
 		if (epsilon)
-			src.add("ε");
+			src.add(CommProcess.Epsilon);
 	}
 
 	/**
@@ -123,28 +116,25 @@ public class First {
 	 * @return
 	 */
 	private void analyse() {
-		for (Entry<String, String[]> prod : pros.entrySet()) {// 遍历每一个非终结符
-			for (String p : prod.getValue()) {// 遍历对应的每一个产生式
-				TokenProcesser tr = new TokenProcesser(
-						p);
-				while (tr.hasNextToken()) {
-					String t = tr.nextToken();
-					if (CommProcess.IsTerminal(t)) {// 终结符
-						if (t.equals("ε") && tr.hasNextToken()) {// t为ε，但不是最后一个终结符，所以不加入。
+		for (int prod = 0;prod < G.prod.length; prod++) {// 遍历每一个非终结符
+			for (Integer[] p : G.prod[prod]) {// 遍历对应的每一个产生式
+				for (int i=0;i<p.length;i++) {
+					if(p[i]<0){// 终结符
+						if (p[i]==CommProcess.Epsilon && i!=p.length-1) {// t为ε，但不是最后一个终结符，所以不加入。
 							continue;
 						}
-						first_set.get(prod.getKey()).add(t);// 向first集合中添加元素
+						first_set.get(prod).add(-p[i]);// 向first集合中添加元素
 						break;
-					} else {// 非终结符，一般是第一个非终结符，或者之前的非终结符都可能为ε
+					}else{// 非终结符，一般是第一个非终结符，或者之前的非终结符都可能为ε
 						// 添加依赖项
-						if (!rela.containsKey(t)) {
-							rela.put(t, new HashSet<String>());
+						if (!rela.containsKey(p[i])) {
+							rela.put(p[i], new HashSet<Integer>());
 						}
-						rela.get(t).add(prod.getKey());// 此依赖于t
-						if (!_inferUnTermEpsilon(t)) {// 此非终结符不可能为ε，不必再向下搜索了
+						rela.get(p[i]).add(prod);// 此依赖于t
+						if (!_inferUnTermEpsilon(p[i])) {// 此非终结符不可能为ε，不必再向下搜索了
 							break;
-						} else if (!tr.hasNextToken()) {// 即最后一个非终结符，且可能为空
-							first_set.get(prod.getKey()).add("ε");
+						} else if (i==p.length-1) {// 即最后一个非终结符，且可能为空
+							first_set.get(prod).add(CommProcess.Epsilon);
 						}
 					}
 				}
@@ -157,18 +147,18 @@ public class First {
 	 * 根据依赖项填充各first集
 	 */
 	private void fillSetByDependency() {
-		LinkedList<String> depend_list = SetOpSort(rela);
-		for (String it : depend_list) {
-			Set<String> its = first_set.get(it);
-			boolean epsilon = its.contains("ε");
+		LinkedList<Integer> depend_list = SetOpSort(rela);
+		for (Integer it : depend_list) {
+			Set<Integer> its = first_set.get(it);
+			boolean epsilon = its.contains(CommProcess.Epsilon);
 			if (epsilon)
-				its.remove("ε");// 暂时移除ε
-			for (String s : rela.get(it)) {// 依赖于node的节点
+				its.remove(CommProcess.Epsilon);// 暂时移除ε
+			for (Integer s : rela.get(it)) {// 依赖于node的节点
 				// first(it)-{ε}<-first(s)
 				first_set.get(s).addAll(its);// 将it集合的元素合并到s中，当然这里要考虑是否包含ε
 			}
 			if (epsilon)
-				its.add("ε");
+				its.add(CommProcess.Epsilon);
 		}
 	}
 
@@ -179,13 +169,13 @@ public class First {
 	 * @param rela
 	 * @return
 	 */
-	public static LinkedList<String> SetOpSort(Map<String, Set<String>> rela) {
-		LinkedList<String> depend_list = new LinkedList<>();
-		Map<String, Boolean> his = new HashMap<String, Boolean>();
-		for (String s : rela.keySet()) {
+	public static LinkedList<Integer> SetOpSort(Map<Integer, Set<Integer>> rela) {
+		LinkedList<Integer> depend_list = new LinkedList<>();
+		Map<Integer, Boolean> his = new HashMap<Integer, Boolean>();
+		for (Integer s : rela.keySet()) {
 			his.put(s, false);
 		}
-		for (String s : rela.keySet()) {
+		for (Integer s : rela.keySet()) {
 			dependby(s, his, rela, depend_list);
 		}
 		return depend_list;
@@ -198,12 +188,12 @@ public class First {
 	 * @param his
 	 * @param node
 	 */
-	private static void dependby(String node, Map<String, Boolean> his,
-			Map<String, Set<String>> rela, LinkedList<String> depend_list) {
+	private static void dependby(Integer node, Map<Integer, Boolean> his,
+			Map<Integer, Set<Integer>> rela, LinkedList<Integer> depend_list) {
 		if (!his.containsKey(node) || his.get(node))// 被访问过，回溯
 			return;
 		his.put(node, true);
-		for (String s : rela.get(node)) {// 依赖于node的节点
+		for (Integer s : rela.get(node)) {// 依赖于node的节点
 			dependby(s, his, rela, depend_list);
 		}
 		// 完成了所有的依赖才能够添加
@@ -216,8 +206,8 @@ public class First {
 	 * @param T
 	 * @return
 	 */
-	public boolean inferUnTermEpsilon(String T) {
-		return first_set.get(T).contains("ε");
+	public boolean inferUnTermEpsilon(int T) {
+		return first_set.get(T).contains(CommProcess.Epsilon);
 	}
 
 	/**
@@ -227,7 +217,7 @@ public class First {
 	 * @param T
 	 * @return
 	 */
-	private boolean _inferUnTermEpsilon(String T) {
+	private boolean _inferUnTermEpsilon(int T) {
 		if (isEpsilon.get(T) != null) {
 			return isEpsilon.get(T);
 		}
@@ -235,51 +225,25 @@ public class First {
 			isEpsilon.put(T, true);
 			return true;
 		}
-		String[] pro = pros.get(T);
-		outer: for (String p : pro) {
-			TokenProcesser tr = new TokenProcesser(p);
-			while (tr.hasNextToken()) {
-				String t = tr.nextToken();
-				if (!CommProcess.IsTerminal(t)) {// 非终结符
-					if (!_inferUnTermEpsilon(t)) {// 其中一个非终结符不可能为空则不可能为空
+		Integer[][] pro = G.prod[T];
+		outer: for (Integer[] p : pro) {
+			for (Integer t : p) {
+				if(t<0){// 终结符
+					if (t!=CommProcess.Epsilon) {// 非ε终结符，则不可能为空
 						continue outer;
 					}
-				} else {// 终结符
-					if (!t.equals("ε")) {// 非ε终结符，则不可能为空
+				}else{// 非终结符
+					if (!_inferUnTermEpsilon(t)) {// 其中一个非终结符不可能为空则不可能为空
 						continue outer;
 					}
 				}
 			}
-			first_set.get(T).add("ε");
+			first_set.get(T).add(CommProcess.Epsilon);
 			isEpsilon.put(T, true);
 			return true;// 如果不可能，则一定跳转到下次循环进行查找
 		}
 		isEpsilon.put(T, false);
 		return false;// 如果可能，则一定在前面的查找中找到返回了。
 	}
-
-	// /**
-	// * 推断字串是否为空
-	// *
-	// * @param T
-	// * @return
-	// */
-	// boolean inferStringEpsilon(String B) {
-	// int index = 0;
-	// while (index < B.length()) {
-	// String t = getNextToken(B, index);
-	// index += t.length();
-	// if (!IsTerminal(t)) {// 非终结符
-	// if (!_inferUnTermEpsilon(t)) {// 其中一个非终结符不可能为空则不可能为空
-	// return false;
-	// }
-	// } else {// 终结符
-	// if (!t.equals("ε")) {// 非ε终结符，则不可能为空
-	// return false;
-	// }
-	// }
-	// }
-	// return true;// 如果不可能，则一定跳转到下次循环进行查找
-	// }
 
 }
